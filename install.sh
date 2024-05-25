3457e173f4706ae60bd520105e787be31c79c82c
nixpig <143995476+nixpig@users.noreply.github.com>
2024-05-25 04:49:35 +0100

    chore: ask for sudo password in install script

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ else
 		sudo pacman -Syu --noconfirm ansible && echo "Ansible installed"
 fi
 
-ansible-pull --checkout main --url=https://github.com/nixpig/playbook.git --ask-vault-pass
+ansible-pull --checkout main --url=https://github.com/nixpig/playbook.git --ask-vault-pass --ask-become-pass
 
